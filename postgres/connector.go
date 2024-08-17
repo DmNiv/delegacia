1bae5ec75dfabcd6ed778cc223d3545ce51d69d5
Logan Cardoso <logan.cc@outlook.com>
2024-08-17 17:33:00 -0300

    fix: ajustes na url de conexão

--- postgres/connector.go
+++ postgres/connector.go
@@ -13,7 +13,8 @@ func InitDB() *gorm.DB {
 	var err error
 	fmt.Println("Connecting to PostgreSQL...")
 
-	dsn := "host=localhost user=labsc password=phishingquest dbname=phishing_quest port=5432 sslmode=disable search_path=phishing_quest"
+	dsn := "host=localhost user=oficina-futuro password=delegaciafacil dbname=delegacia-facil port=5432 sslmode=disable search_path=delegacia_facil"
+
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
