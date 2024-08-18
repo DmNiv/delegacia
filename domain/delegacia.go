8702c192d5595d4c23a6c8e6bfa3d393417548a3
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2024-08-18 13:18:18 -0300

    fix: atualizando model Delegacia

--- domain/delegacia.go
+++ domain/delegacia.go
@@ -3,10 +3,11 @@ package domain
 import "github.com/google/uuid"
 
 type Delegacia struct {
-	Id         uuid.UUID `json:"id" gorm:"primary_key"`
-	Nome       string    `json:"nome"`
-	Endereco   string    `json:"endereco"`
-	Horario24h bool      `json:"horario24h"`
-	Latitude   float64   `json:"latitude"`
-	Longitude  float64   `json:"longitude"`
+	Id        uuid.UUID `json:"id" gorm:"primary_key"`
+	Nome      string    `json:"nome"`
+	Endereco  string    `json:"endereco"`
+	DiaTodo   bool      `json:"diaTodo"`
+	Latitude  float64   `json:"latitude"`
+	Longitude float64   `json:"longitude"`
+	MapUrl    string    `json:"mapUrl"`
 }
